fad0d5c8b0c735d5b2557bcde8c3f4af3cf7b7dd
VacodaC <56671882+VacodaC@users.noreply.github.com>
2019-10-18 00:01:34 +1000

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,15 +1,33 @@
-var callback = function(info) {
-		var redir = info.url
-		if (info.url.includes("?refer=raid"))
-		{
-			redir = info.url.substring(0, info.url.length - 11);
-		}
-		if (info.url.includes("?referrer=raid"))
-		{
-			redir = info.url.substring(0, info.url.length - 14);
-		}
-		return {redirectUrl: redir};
-	};
 var filter = {urls: ['*://twitch.tv/*', '*://www.twitch.tv/*']};
 var specInfo = ['blocking'];
-chrome.webRequest.onBeforeRequest.addListener(callback, filter, specInfo);
+var callbackOnRequest = function(info) {
+	var redir = info.url
+	if (info.url.includes("?refer=raid"))
+	{
+		redir = info.url.substring(0, info.url.length - 11);
+	}
+	if (info.url.includes("?referrer=raid"))
+	{
+		redir = info.url.substring(0, info.url.length - 14);
+	}
+	return {redirectUrl: redir};
+};
+chrome.webRequest.onBeforeRequest.addListener(callbackOnRequest, filter, specInfo);
+var callbackOnComplete = function(info){
+	var redir = info.url
+	if (info.url.includes("?refer=raid"))
+	{
+		redir = info.url.substring(0, info.url.length - 11);
+	}
+	if (info.url.includes("?referrer=raid"))
+	{
+		redir = info.url.substring(0, info.url.length - 14);
+	}
+	if(redir != info.url)
+	{
+		chrome.tabs.update(info.tabId, {url: redir});
+	}
+	return;
+};
+chrome.webNavigation.onCompleted.addListener(callbackOnComplete, {url: [{hostSuffix: 'twitch.tv'}]});
+chrome.webNavigation.onHistoryStateUpdated.addListener(callbackOnComplete, {url: [{hostSuffix: 'twitch.tv'}]});
