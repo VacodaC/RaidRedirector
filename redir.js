d945af94e4dd33caf322dd2f79c2dfd6d8429aa9
VacodaC <56671882+VacodaC@users.noreply.github.com>
2019-10-17 17:35:26 +1000

    Add files via upload

---
+++ redir.js
@@ -0,0 +1,11 @@
+var callback = function(info) {
+		var redir = info.url
+		if (info.url.includes("?refer=raid"))
+		{
+			redir = info.url.substring(0, info.url.length - 11);
+		}
+		return {redirectUrl: redir};
+	};
+var filter = {urls: ['*://twitch.tv/*', '*://www.twitch.tv/*']};
+var specInfo = ['blocking'];
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, specInfo);
